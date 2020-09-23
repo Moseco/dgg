35d291a974023ca46b65c85c126f6c1b202526f7
Hampus Hammarlund <hampus_hammarlund@hotmail.com>
2020-09-23 12:50:57 +0900

    Fixed handling of hidden property for falirs

--- lib/datamodels/flairs.dart
+++ lib/datamodels/flairs.dart
@@ -12,9 +12,7 @@ class Flairs {
 
     List<Flair> flairs = List();
     json.forEach((element) {
-      if (!element['hidden']) {
-        flairs.add(Flair.fromJson(element));
-      }
+      flairs.add(Flair.fromJson(element));
     });
     return Flairs(flairs);
   }
@@ -40,7 +38,7 @@ class Flair {
       map['color'].length == 0
           ? null
           : int.parse('FF' + map['color']?.substring(1), radix: 16),
-      map['image'][0]['url'],
+      map['hidden'] ? null : map['image'][0]['url'],
     );
   }
 }
