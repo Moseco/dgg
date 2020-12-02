79bedf432262211dea1dec29703f63b4cddcf9b9
Hampus Hammarlund <hampus_hammarlund@hotmail.com>
2020-12-02 20:29:00 +0900

    Improved home screen

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:dgg/datamodels/session_info.dart';
 import 'package:dgg/ui/widgets/delayed_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:stacked/stacked.dart';
 
 import 'home_viewmodel.dart';
@@ -16,16 +17,17 @@ class HomeView extends StatelessWidget {
       fireOnModelReadyOnce: true,
       builder: (context, model, child) => Scaffold(
         body: SafeArea(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: SingleChildScrollView(
               child: Column(
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(32),
                     child: Image.asset(
-                      "assets/dgg_icon.png",
-                      width: 150,
+                      "assets/dgg_logo.png",
+                      width: MediaQuery.of(context).size.width * 0.75,
                     ),
                   ),
                   model.sessionInfo == null
@@ -56,14 +58,22 @@ class HomeView extends StatelessWidget {
   }
 
   Widget _buildLoaded(BuildContext context, HomeViewModel model) {
-    return Column(
-      children: [
-        _buildSessionWidget(model),
-        RaisedButton(
-          child: Text("Go to chat"),
-          onPressed: () => model.navigateToChat(),
+    return AnimationLimiter(
+      child: Column(
+        children: AnimationConfiguration.toStaggeredList(
+          duration: const Duration(milliseconds: 375),
+          childAnimationBuilder: (widget) => SlideAnimation(
+            verticalOffset: 50.0,
+            child: FadeInAnimation(
+              child: widget,
+            ),
+          ),
+          children: [
+            _buildSessionWidget(model),
+            _buildChatButton(context, model),
+          ],
         ),
-      ],
+      ),
     );
   }
 
@@ -72,15 +82,52 @@ class HomeView extends StatelessWidget {
 
     if (sessionInfo is Unavailable) {
       //Not signed or some kind of error
-      return RaisedButton(
+      return OutlineButton(
+        borderSide: BorderSide(color: Colors.white),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
         child: Text("Sign in"),
         onPressed: () => model.navigateToAuth(),
       );
     } else if (sessionInfo is Available) {
       //User is signed in
-      return Text("Signed in as: ${sessionInfo.nick}");
+      return Text(
+        "Signed in as: ${sessionInfo.nick}",
+        textAlign: TextAlign.center,
+      );
     } else {
       return Text("Error");
     }
+  }
+
+  Widget _buildChatButton(BuildContext context, HomeViewModel model) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 32),
+      child: RaisedButton(
+        color: Theme.of(context).primaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.chat,
+              ),
+              Container(height: 4),
+              Text(
+                "Open chat",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+        onPressed: () => model.navigateToChat(),
+      ),
+    );
   }
 }
