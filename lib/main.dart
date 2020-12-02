66ff55fec13ce34692c5697f054a1dbd7606d2cf
Hampus Hammarlund <hampus_hammarlund@hotmail.com>
2020-12-02 21:12:18 +0900

    Added firebase to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 import 'app/locator.dart';
 import 'app/router.gr.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   setupLocator();
   runApp(App());
 }
