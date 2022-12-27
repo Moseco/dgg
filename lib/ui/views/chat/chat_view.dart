50e195c3d48e94a321a7a4ce4d3cfa813aee0d2d
Riccardo Mancini <riccardo.mancini01@gmail.com>
2022-12-27 04:38:05 +0100

    Remove useless import

--- lib/ui/views/chat/chat_view.dart
+++ lib/ui/views/chat/chat_view.dart
@@ -1,5 +1,4 @@
 import 'package:dgg/ui/views/chat/widgets/emote_selector.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:stacked/stacked.dart';
